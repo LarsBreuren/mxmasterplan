4ebcc6a0501da86f0a04a36e943fd86626862db8
LarsBreuren <larsbreuren@gmail.com>
2022-02-22 11:16:33 +0100

    News page WIP

--- news.php
+++ news.php
@@ -17,39 +17,65 @@
 
       <div class="newsContainer">
       <div class="container">
-      <h2 class="newsTitle">MX nieuws</h2>
-      <div class="featuredItems">
-        <?php 
-          $posts = get_posts(array(
-            'posts_per_page'	=> 2,
-            'post_type'			=> 'post',
-            'category_name' => 'nieuws', 
-          ));
-          if( $posts ): ?>	
-                <?php foreach( $posts as $post ):
-                    setup_postdata( $post );
-                    if(  get_field( "featured_article" ) == "Featured"){
-                    $cat = get_the_category($post->ID);
-                    $mainText = get_field( "hoofdtekst" );
-                    ?>
-                      <a href="<?php echo the_permalink() ?>" class="featuredItem">
-                        <div class="thumbImg" style=' 
-                          background-image: url("<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())) ?>");
-                          background-position:<?php echo get_field( "thumb_alignment" ); ?>;'>
-                        </div>
-                      <div class="opacity"></div>
-                      <div class="content">
-                        <h3><?php the_title(); ?></h3>
-                    </div>
-                    <div class="date"><p><?php echo get_the_date( 'd-m-y' ); ?></p></div>
-                    </a>
-              <?php } endforeach; ?>
-            <?php wp_reset_postdata(); ?>
-          <?php endif; ?>
+        <h2 class="newsTitle">MX nieuws</h2>
+        <div class="featuredItems">
+          <?php 
+            $posts = get_posts(array(
+              'post_type'			=> 'post',
+              'category_name' => 'nieuws', 
+            ));
+            if( $posts ): ?>	
+                  <?php foreach( $posts as $post ):
+                      setup_postdata( $post );
+                      if(  get_field( "featured_article" ) == "Featured"){
+                      $cat = get_the_category($post->ID);
+                      $mainText = get_field( "hoofdtekst" );
+                      ?>
+                        <a href="<?php echo the_permalink() ?>" class="featuredItem">
+                          <div class="thumbImg" style=' 
+                            background-image: url("<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())) ?>");
+                            background-position:<?php echo get_field( "thumb_alignment" ); ?>;'>
+                          </div>
+                        <div class="opacity"></div>
+                        <div class="content">
+                          <h3><?php the_title(); ?></h3>
+                      </div>
+                      <div class="date"><p><?php echo get_the_date( 'd-m-y' ); ?></p></div>
+                      </a>
+                <?php } endforeach; ?>
+              <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+            </div>
+            <div class="newsItems"> 
+            <?php 
+            $posts = get_posts(array(
+              'post_type'			=> 'post',
+              'category_name' => 'nieuws', 
+            ));
+            if( $posts ): ?>	
+                  <?php foreach( $posts as $post ):
+                      setup_postdata( $post );
+                      if(  get_field( "featured_article" ) != "Featured"){
+                      $cat = get_the_category($post->ID);
+                      $mainText = get_field( "hoofdtekst" );
+                      ?>
+                        <a href="<?php echo the_permalink() ?>" class="newsItem">
+                          <div class="thumbImg" style=' 
+                            background-image: url("<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())) ?>");
+                            background-position:<?php echo get_field( "thumb_alignment" ); ?>;'>
+                          </div>
+                        <div class="opacity"></div>
+                        <div class="content">
+                          <h3><?php the_title(); ?></h3>
+                      </div>
+                      <div class="date"><p><?php echo get_the_date( 'd-m-y' ); ?></p></div>
+                      </a>
+                <?php } endforeach; ?>
+              <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
           </div>
+          
       </div>
     </div>
-
-
 <?php    get_footer();
     ?>
