a5a6330093d4fb0bff1a807b1225ea160149b88a
LarsBreuren <larsbreuren@gmail.com>
2022-03-03 15:53:07 +0100

    Footer added

--- footer.php
+++ footer.php
@@ -1,6 +1,20 @@
 <footer>
-
-    </footer>
+    <div><img class="mountain" src="<?php echo get_theme_file_uri('/images/mountain.svg') ?>"></div>
+    <div class="footerContent">
+        <div><img class="logo" src="<?php echo get_theme_file_uri('/images/logo-white.svg') ?>"></div>
+        <div class="links">
+            <ul>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">News</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <li><a href="about.asp">About</a></li>
+            </ul> 
+        </div>
+        <div class="socials">
+            <P> Enige links naar socials</P>
+        </div>
+    </div>
+</footer>
     <?php wp_footer(); ?>
 </body>
 </html>
--- single.php
+++ single.php
@@ -11,7 +11,10 @@
                 <div class="slogan"><h1><?php echo get_the_title(); ?></h1></div>
                 </div>
               </div>
-              <div class="heroImage" style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())) ?>');"> </div>
+              <div class="heroImage" style="
+              background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())) ?>');
+              background-position:<?php echo get_field( "thumb_alignment" ); ?>;'>
+              "> </div>
               <img class="mountain" src="<?php echo get_theme_file_uri('/images/mountain.svg') ?>">
          </div>
          <div class="container">
