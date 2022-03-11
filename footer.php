6ef93842b8494c2c364b0fdc96af46b77c9cde25
LarsBreuren <larsbreuren@gmail.com>
2022-03-11 12:38:45 +0100

    Footer responsive improved

--- footer.php
+++ footer.php
@@ -1,17 +1,21 @@
 <footer>
-    <div><img class="mountain" src="<?php echo get_theme_file_uri('/images/mountain.svg') ?>"></div>
-    <div class="footerContent">
-        <div><img class="logo" src="<?php echo get_theme_file_uri('/images/logo-white.svg') ?>"></div>
-        <div class="links">
-            <ul>
-                <li><a href="default.asp">Home</a></li>
-                <li><a href="news.asp">News</a></li>
-                <li><a href="contact.asp">Contact</a></li>
-                <li><a href="about.asp">About</a></li>
-            </ul> 
-        </div>
-        <div class="socials">
-            <P> Enige links naar socials</P>
+    <div class="mountainContainer"><div class="mountain" style="background-image: url('<?php echo get_theme_file_uri('/images/mountain.svg') ?>');"> </div></div>
+    <div class="container">
+        <div class="footerContent">
+            <div><img class="logo" src="<?php echo get_theme_file_uri('/images/logo-white.svg') ?>"></div>
+            <div class="links">
+                <ul>
+                    <li><a href="default.asp">Home</a></li>
+                    <li><a href="news.asp">News</a></li>
+                    <li><a href="contact.asp">Contact</a></li>
+                    <li><a href="about.asp">About</a></li>
+                </ul> 
+            </div>
+            <div class="socials">
+                <P> Enige links naar socials</P>
+                <p><span class="material-icons">instagram</span></p>
+               <p><span class="material-icons">sports_score </span></p> 
+            </div>
         </div>
     </div>
 </footer>
