69c5209e4050337c5e855a356217f0c1db92614c
LarsBreuren <larsbreuren@gmail.com>
2022-03-22 12:05:00 +0100

    Faq has an active button now

--- faq.php
+++ faq.php
@@ -12,31 +12,30 @@ get_header(); ?>
     <img class="mountain" src="<?php echo get_theme_file_uri('/images/mountain.svg') ?>">
 </div>
 
-    <div class="container faq">
-     <h1> Veelgestelde vragen</h1>
-     <?php
-        $fields = get_field_objects( 148 );
-        $counter = 1;
-        if( $fields ): ?>
-                <?php foreach( $fields as $field ): ?>
-                    <?php if( have_rows($field['label']) ): ?>
-                        <?php while( have_rows($field['label']) ): the_row(); 
-                            // Get sub field values.
-                            $question= get_sub_field('vraag');
-                            $answer = get_sub_field('antwoord');
-                            ?>
+<div class="container faq">
+    <h1> Veelgestelde vragen</h1>
+    <?php
+    $fields = get_field_objects( 148 );
+    $counter = 1;
+    if( $fields ): ?>
+            <?php foreach( $fields as $field ): ?>
+                <?php if( have_rows($field['label']) ): ?>
+                    <?php while( have_rows($field['label']) ): the_row(); 
+                        // Get sub field values.
+                        $question= get_sub_field('vraag');
+                        $answer = get_sub_field('antwoord');
+                        $active = get_sub_field('actief');
+                        if ($active == '1'){ ?>
                             <div id="<?php echo $counter ?>" class="questionContainer">
-                                    <div class="question"><p><?php echo $question?></p> </div>
-                                   <div class="chevron"><img src="<?php echo get_theme_file_uri('/images/icons/chevron-up.svg') ?>"></div> 
-                                 <div class="answer"><p><?php echo $answer ?> </p></div>
+                                <div class="question"><p><?php echo $question?></p> </div>
+                                <div class="chevron"><img src="<?php echo get_theme_file_uri('/images/icons/chevron-up.svg') ?>"></div> 
+                                <div class="answer"><p><?php echo $answer ?> </p></div>
                             </div>
-                        <?php  endwhile; ?>
-                    <?php endif; ?>
-                <?php $counter++; endforeach; ?>
-        <?php endif; ?>
-    </div>
+                        <?php }; ?>
+                    <?php  endwhile; ?>
+                <?php endif; ?>
+            <?php $counter++; endforeach; ?>
+    <?php endif; ?>
+</div>
     
 <?php  get_footer();?>
-
-
-
