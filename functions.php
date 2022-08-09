ec2a9fb334c12a0d13fafe50a034fcdacbfe05e7
LarsBreuren <larsbreuren@gmail.com>
2022-08-09 14:56:44 +0200

    Added inc's for functions.php

--- functions.php
+++ functions.php
@@ -1,20 +1,20 @@
 <?php
 
-function university_files() {
+function mx_setup() {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('our-main-styles', get_theme_file_uri('/dist/styles.css'));
 }
 
-    add_action('wp_enqueue_scripts', 'university_files');
+    add_action('wp_enqueue_scripts', 'mx_setup');
 
-    function university_features(){
+    function mx_features(){
         add_theme_support('title-tag');
     }
-    add_action('after_setup_theme', 'university_features');
+    add_action('after_setup_theme', 'mx_features');
 
     add_theme_support( 'post-thumbnails' );
 
-    function my_custom_menu() {
+    function mx_custom_menu() {
       register_nav_menus(
           array(
               'my-custom-menu' => _( 'My Custom Menu' ),
@@ -22,24 +22,11 @@ function university_files() {
           )
       );
   }
-  add_action( 'init', 'my_custom_menu' );
+  add_action( 'init', 'mx_custom_menu' );
 
-// Only use faq.js on the faq page for improved performance
-       function my_scripts() {
-       
-        if( is_page( array( 'FAQ' ) ) ){
-          wp_enqueue_script('my-script', get_template_directory_uri() .'/assets/js/faq.js');	
-        }
-        else if( is_page( array( 'Projecten' ) ) ){
-          wp_enqueue_script('pie-chart', get_template_directory_uri() .'/assets/js/pieChart.js', array('jquery'), null, true); 
-        }
-        if( is_page( array( 'is_front_page()' ) ) ){
-        wp_enqueue_script('counter', get_template_directory_uri() .'/assets/js/count.js');	
-        }
-        wp_enqueue_script('mobileMenu', get_template_directory_uri() .'/assets/js/mobileMenu.js');	
-    }
-    add_action( 'wp_enqueue_scripts', 'my_scripts' );
+/**
+ * Loading custom scripts
+ */
+require get_template_directory() . '/inc/scripts-loader.php';
+
 ?>
-
-
-
---
+++ inc/scripts-loader.php
@@ -0,0 +1,16 @@
+<?php
+       function my_scripts() {
+       
+        if( is_page( array( 'FAQ' ) ) ){
+          wp_enqueue_script('my-script', get_template_directory_uri() .'/assets/js/faq.js');	
+        }
+        else if( is_page( array( 'Projecten' ) ) ){
+          wp_enqueue_script('pie-chart', get_template_directory_uri() .'/assets/js/pieChart.js', array('jquery'), null, true); 
+        }
+        if( is_page( array( 'is_front_page()' ) ) ){
+        wp_enqueue_script('counter', get_template_directory_uri() .'/assets/js/count.js');	
+        }
+        wp_enqueue_script('mobileMenu', get_template_directory_uri() .'/assets/js/mobileMenu.js');	
+    }
+    add_action( 'wp_enqueue_scripts', 'my_scripts' );
+?>
